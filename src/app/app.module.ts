a884830077cdca15e137a45fc3510548afde874d
Eduardo Contessi <Eduardo.Contessi@thomsonreuters.com>
2019-05-20 10:26:29 -0300

    Capítulo 4.1 Interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 
 import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
 import { AlertaService } from './alerta.service';
+import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaPessoaComponent
+    ListaPessoaComponent,
+    InterpolationBindingComponent
   ],
   imports: [
     BrowserModule,
