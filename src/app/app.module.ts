5a50c914e132f9a97060cbf81da2c245fb7ade33
Eduardo Contessi <Eduardo.Contessi@thomsonreuters.com>
2019-05-22 16:13:43 -0300

    Capítulo 4.10 NGCONTENT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgClassComponent } from './ng-class/ng-class.component';
 import { NgStyleComponent } from './ng-style/ng-style.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { NgStyleComponent } from './ng-style/ng-style.component';
     NgSwitchCaseComponent,
     NgForComponent,
     NgClassComponent,
-    NgStyleComponent
+    NgStyleComponent,
+    NgContentComponent
   ],
   imports: [
     BrowserModule,
